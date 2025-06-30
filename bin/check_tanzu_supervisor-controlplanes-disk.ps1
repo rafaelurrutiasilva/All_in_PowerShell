fe14ad243627b87685a860d8a8681ca90eb5dbba
Rafael Urrutia Silva <84277015+rafaelurrutiasilva@users.noreply.github.com>
2025-06-30 17:06:11 +0200

    Update check_tanzu_supervisor-controlplanes-disk.ps1

--- bin/check_tanzu_supervisor-controlplanes-disk.ps1
+++ bin/check_tanzu_supervisor-controlplanes-disk.ps1
@@ -1,1 +1,3 @@
-#This PowerShell script retrieves all VMware Tanzu Supervisor Control Plane VMs and checks their disk usage. It outputs results for use as a Nagios check and also sends alerts to syslog if thresholds are exceeded.
+#
+# This PowerShell script connects to a vCenter server, identifies all VMware Tanzu Supervisor Control Plane virtual machines, and examines their disk utilization. It generates output formatted for Nagios monitoring, 
+# raising critical alerts if usage exceeds a configurable threshold (default 80%). Additionally, on Unix systems, it logs any critical events to syslog, enabling integration with broader infrastructure monitoring and alerting tools.
